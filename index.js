4a35995f8c7be515088105dd7fc27669a1d35ab9
Marcelo Souza Lima <marcelo.nalon@corp.globo.com>
2019-04-25 17:03:39 -0300

    Add complexity limit rule

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import { ApolloServer, gql } from 'apollo-server'
 const { RedisCache } = require('apollo-server-cache-redis');
+import { createComplexityLimitRule } from 'graphql-validation-complexity'
 
 import typeDefs from './type-defs'
 import resolvers from './resolvers'
 import { ProgramsDatasource, VideosDatasource } from '../data-sources'
+
+const MAX_QUERY_COMPLEXITY = 450
+
+const costs = {
+  scalarCost: 1,
+  objectCost: 0,
+  listFactor: 10,
+  introspectionListFactor: 1
+}
 
 const server = new ApolloServer({ 
   typeDefs, 
@@ -12,6 +22,7 @@ const server = new ApolloServer({
     host: 'localhost',
     port: 6379
   }),
+  validationRules: [createComplexityLimitRule(MAX_QUERY_COMPLEXITY, costs)], 
   dataSources: () => ({ 
     programs: new ProgramsDatasource(),
     videos: new VideosDatasource() 
