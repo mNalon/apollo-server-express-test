b8c0909169c143e003fd13c8e8429e55166b45cc
Marcelo Souza Lima <marcelo.nalon@corp.globo.com>
2019-04-25 21:56:21 -0300

    Add dynamic cache hint

--- resolvers.js
+++ resolvers.js
@@ -4,10 +4,17 @@ const resolvers = {
   },
 
   Video: {
-    duration: ({ id }, _, { dataSources }) =>
+    duration: ({ id }, variables, { dataSources }) =>
       dataSources.videos.getVideoById(id).then(({ duration }) => duration),
-    thumbnail: ({ id }, _, { dataSources }) => 
+    thumbnail: ({ id }, variables, { dataSources }) => 
       dataSources.videos.getVideoById(id).then(({ thumbnail }) => thumbnail)
+  },
+
+  User: {
+    lastWatchedVideos: (videos, variables, context, { cacheControl }) => {
+      if('something happens...') cacheControl.setCacheHint({ maxAge: 300 })
+      return videos
+    }
   }
 }
 
